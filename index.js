2444980222815a04c5245bb13124823e5de3739d
Shajibul Alam Shihab <shihab77023@gmail.com>
2023-12-10 10:26:33 +0600

    update

--- index.js
+++ index.js
@@ -5,21 +5,24 @@ const app = express();
 const port = process.env.PORT || 5000;
 const errorHandler = require("./middleware/errorHandler");
 const testRoutes = require("./routes/v1/test.route");
+const userRoutes = require("./routes/v1/users.route");
 
-
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('front'));
+app.use(express.static("front"));
 
 // Error handler middleware
 app.use(errorHandler);
 
 // Attach your routes after the error handler
 app.use("/api/v1/test", testRoutes);
+
+// For users
+app.use("/api/v1/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Experiment Labs server is running");
