c9af6f7837744ea94b62ae13523f40247963ba55
Shajibul Alam Shihab <shihab77023@gmail.com>
2023-12-13 09:47:49 +0600

    update

--- controllers/organizations.controller.js
+++ controllers/organizations.controller.js
@@ -7,26 +7,6 @@ const userCollection = client
   .db("ExperimentLabsInternshipPortal")
   .collection("users");
 
-// module.exports.createAnOrganization = async (req, res, next) => {
-//   const user = req.body;
-//   const result = await orgCollection.insertOne(user);
-//   const organizationId = result.insertedId;
-//   const email = user.email;
-//   const filter = { email: email };
-//   const options = { upsert: true };
-
-//   const updatedDoc = {
-//     $set: {
-//       organizationId: "" + organizationId,
-//       organizationName: "" + user?.organizationName,
-//       role: "Admin",
-//     },
-//   };
-
-//   const newResult = await userCollection.updateOne(filter, updatedDoc, options);
-//   res.send({ result, newResult });
-// };
-
 module.exports.createAnOrganization = async (req, res, next) => {
   const org = req.body;
   const result = await orgCollection.insertOne(org);
