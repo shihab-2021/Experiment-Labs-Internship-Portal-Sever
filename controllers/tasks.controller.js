506e1c3cbee67991161af7dcba4d0b840780fe91
Shajibul Alam Shihab <shihab77023@gmail.com>
2023-12-18 13:00:19 +0600

    update

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -3,6 +3,9 @@ const client = require("../utils/dbConnect");
 const taskCollection = client
   .db("ExperimentLabsInternshipPortal")
   .collection("tasks");
+const taskSubmissionCollection = client
+  .db("ExperimentLabsInternshipPortal")
+  .collection("taskSubmissions");
 
 module.exports.saveATask = async (req, res, next) => {
   const task = req.body;
@@ -39,4 +42,119 @@ module.exports.deleteATask = async (req, res, next) => {
   const { id } = req.params;
   const result = await taskCollection.deleteOne({ _id: new ObjectId(id) });
   res.send(result);
+};
+
+module.exports.applyForTask = async (req, res, next) => {
+  const { taskId } = req.params;
+  const { participantEmail, applyDateTime } = req.body;
+
+  try {
+    const taskQuery = { _id: ObjectId(taskId) };
+    const task = await taskCollection.findOne(taskQuery);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    // Check if participants array exists, if not, create it
+    if (!task.participants) {
+      task.participants = [];
+    }
+
+    // Check if participant has already applied
+    const participantAlreadyApplied = task.participants.some(
+      (participant) => participant.participantEmail === participantEmail
+    );
+
+    if (participantAlreadyApplied) {
+      return res
+        .status(400)
+        .json({ message: "Participant has already applied for this task" });
+    }
+
+    // Add participant application to the task
+    const participantInfo = {
+      participantEmail,
+      applyDateTime,
+    };
+
+    task.participants.push(participantInfo);
+
+    // Update task with the new participant application
+    await taskCollection.updateOne(taskQuery, {
+      $set: { participants: task.participants },
+    });
+
+    res
+      .status(200)
+      .json({ message: "Participant applied for the task successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports.submitATask = async (req, res, next) => {
+  const newSubmission = req.body;
+
+  try {
+    const query = {
+      taskId: newSubmission.taskId,
+      participantEmail: newSubmission.participantEmail,
+    };
+
+    let existingSubmission = await taskSubmissionCollection.findOne(query);
+
+    // If there's no existing submission, create a new one
+    if (!existingSubmission) {
+      const insertedSubmission = await taskSubmissionCollection.insertOne(
+        newSubmission
+      );
+      existingSubmission = { _id: insertedSubmission.insertedId };
+    } else {
+      // Update existing submission details
+      await taskSubmissionCollection.updateOne(query, {
+        $set: {
+          aboutSolution: newSubmission.aboutSolution,
+          fileLink: newSubmission.fileLink,
+          submissionDateTime: newSubmission.submissionDateTime,
+        },
+      });
+    }
+
+    const taskQuery = { _id: ObjectId(newSubmission.taskId) };
+    const task = await taskCollection.findOne(taskQuery);
+
+    if (task) {
+      const participantIndex = task.participants?.findIndex(
+        (participant) =>
+          participant.participantEmail === newSubmission.participantEmail
+      );
+
+      if (participantIndex !== -1) {
+        // Add submissionId and submissionDateTime to the participant object
+        task.participants[participantIndex].submissionId = String(
+          existingSubmission._id
+        ); // Convert ObjectId to string
+        task.participants[participantIndex].submissionDateTime =
+          newSubmission.submissionDateTime;
+
+        // Update task with the new submission details
+        await taskCollection.updateOne(taskQuery, {
+          $set: { participants: task.participants },
+        });
+
+        return res
+          .status(200)
+          .json({ message: "Submission handled successfully" });
+      }
+    }
+
+    return res.status(400).json({
+      message: "Participant must apply for the task before submitting",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
