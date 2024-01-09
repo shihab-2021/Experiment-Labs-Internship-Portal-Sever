812c079f4bbecd7857aa4adfbd83bde62822b3c3
Shajibul Alam Shihab <shihab77023@gmail.com>
2024-01-09 21:18:34 +0600

    update

--- controllers/schools.controller.js
+++ controllers/schools.controller.js
@@ -1,11 +1,18 @@
 const { ObjectId } = require("mongodb");
 const client = require("../utils/dbConnect");
 const schoolCollection = client
-    .db("ExperimentLabsInternshipPortal")
-    .collection("schools");
+  .db("ExperimentLabsInternshipPortal")
+  .collection("schools");
 
 module.exports.getSchoolsByCounsellorId = async (req, res, next) => {
-    const {counsellorId} = req.params;
-    const result = await schoolCollection.find({counsellorId}).toArray();
-    res.send(result);
+  const { counsellorId } = req.params;
+  const result = await schoolCollection.find({ counsellorId }).toArray();
+  res.send(result);
+};
+
+module.exports.getASchoolById = async (req, res, next) => {
+  const { schoolId } = req.params;
+  const query = { _id: new ObjectId(schoolId) };
+  const school = await schoolCollection.findOne(query);
+  res.send(school);
 };
